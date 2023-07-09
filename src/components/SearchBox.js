21a18dbc4f3703e5ca62dd0072db7b6029b27803
Arif Rahman <arif.rahman.rhm@gmail.com>
2023-07-09 21:33:40 +0700

    WIP: Fix search

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,17 +1,40 @@
-import React, { useState } from "react";
+import { useState, useReducer } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
-import { useNavigate } from "react-router-dom";
+
+import axios from 'axios';
+
+// Define the reducer function
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, data: action.payload };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+}
 
 export default function SearchBox() {
-  const navigate = useNavigate();
+  const initialState = { loading: false, data: null, error: null };
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [query, setQuery] = useState("");
-  const submitHandler = (e) => {
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    navigate(query ? `/search/?query=${query}` : "/search");
-  };
+    dispatch({ type: "FETCH_REQUEST" });
+    try {
+      const result = await axios.get("/products/search?query=" + query);
+      dispatch({ type: "FETCH_SUCCESS", payload: result.data })
+    } catch (err) {
+      dispatch({ type: "FETCH_FAIL", payload: err.message })
+    }
+  }
 
   return (
     <Form className="d-flex me-auto" onSubmit={submitHandler}>
@@ -24,7 +47,7 @@ export default function SearchBox() {
           placeholder="search products..."
           aria-label="Search Products"
           aria-describedby="button-search"
-        ></FormControl>
+        />
         <Button variant="outline-primary" type="submit" id="button-search">
           <i className="fas fa-search"></i>
         </Button>
