ae35315746e56b6552ceff27702a5f5171a88ea2
Arif Rahman <arif.rahman.rhm@gmail.com>
2023-07-20 21:22:46 +0700

    WIP : Fix Search

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,39 +1,18 @@
-import { useState, useReducer } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import { useNavigate } from "react-router-dom";
 
-import axios from 'axios';
-
-// Define the reducer function
-function reducer(state, action) {
-  switch (action.type) {
-    case "FETCH_REQUEST":
-      return { ...state, loading: true };
-    case "FETCH_SUCCESS":
-      return { ...state, loading: false, data: action.payload };
-    case "FETCH_FAIL":
-      return { ...state, loading: false, error: action.payload };
-    default:
-      return state;
-  }
-}
 
 export default function SearchBox() {
-  const initialState = { loading: false, data: null, error: null };
-  const [state, dispatch] = useReducer(reducer, initialState);
   const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    dispatch({ type: "FETCH_REQUEST" });
-    try {
-      const result = await axios.get("/products/search?query=" + query);
-      dispatch({ type: "FETCH_SUCCESS", payload: result.data })
-    } catch (err) {
-      dispatch({ type: "FETCH_FAIL", payload: err.message })
-    }
+    navigate(query ? `/products/search?query=${query}` : "/search");
   }
 
   return (
